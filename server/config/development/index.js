029f1053affe407130a1c0e6305a8d508360bc01
david-vaclavek <vaclavek.dvd@gmail.com>
2022-07-05 14:24:45 +0200

    fix testing client id and connector url

--- server/config/development/index.js
+++ server/config/development/index.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   LOCALAZY_OAUTH_URL: "https://testing.localazy.com/oauth/authorize",
-  LOCALAZY_OAUTH_APP_CLIENT_ID: "18503917571895066465",
-  LOCALAZY_PLUGIN_CONNECTOR_API_URL: "http://localhost:3000",
+  LOCALAZY_OAUTH_APP_CLIENT_ID: "18503917571895066466",
+  LOCALAZY_PLUGIN_CONNECTOR_API_URL: "https://testing.localazy.com/plugin-connector",
   LOCALAZY_PUBLIC_API_URL: "https://testing.localazy.com/pubapi",
   LOCALAZY_PLUGIN_ID: "1",
   LOCALAZY_DEFAULT_FILE_NAME: "strapi.json",
