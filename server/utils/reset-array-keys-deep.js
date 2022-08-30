8c93aba15dd9b82ec036dd51b3b9860692805b49
david-vaclavek <vaclavek.dvd@gmail.com>
2022-08-30 18:07:16 +0200

    fix variable definition

--- server/utils/reset-array-keys-deep.js
+++ server/utils/reset-array-keys-deep.js
@@ -2,7 +2,7 @@ const set = require("lodash/set");
 const get = require("lodash/get");
 
 const resetArrayKeysDeep = (object, keysToFilter) => {
-  for (key of keysToFilter) {
+  for (const key of keysToFilter) {
     const part = get(object, key);
     const filteredPart = part.filter((item) => item !== undefined);
     set(object, key, filteredPart);
