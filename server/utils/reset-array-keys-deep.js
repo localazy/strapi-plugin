7fc5d64165d85e98afeecbe36f67d10cf5573ae4
david-vaclavek <vaclavek.dvd@gmail.com>
2022-08-31 12:59:18 +0200

    fix non-matching regex

--- server/utils/reset-array-keys-deep.js
+++ server/utils/reset-array-keys-deep.js
@@ -2,7 +2,14 @@ const set = require("lodash/set");
 const get = require("lodash/get");
 
 const resetArrayKeysDeep = (object, keysToFilter) => {
-  for (const key of keysToFilter) {
+  // sort keys from deep to shallow (so we won't rewrite indices)
+  const sortedKeysToFilter = keysToFilter.sort((a, b) => {
+    const aLvl = a.match(/\./g)?.length || 0;
+    const bLvl = b.match(/\./g)?.length || 0;
+
+    return bLvl - aLvl;
+  })
+  for (const key of sortedKeysToFilter) {
     const part = get(object, key);
     const filteredPart = part.filter((item) => item !== undefined);
     set(object, key, filteredPart);
