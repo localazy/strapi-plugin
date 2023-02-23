d19dd58b996ee3f75e7f8ae810f14f93271f7d74
Guillaume R <guillaume@recursive.fr>
2023-02-23 16:50:29 +0100

    fixed entryId collision + filtering in keyOfEntry

--- server/utils/pick-entries.js
+++ server/utils/pick-entries.js
@@ -12,8 +12,16 @@ const isDynamicZoneKey = (key, transferSetupModel) => {
   }
 
   const dynamicPropertyEntryIdWithSquareBrackets = matches[1];
-  const dynamicPropertyNamePart = key.split(".").find((s, index) => index > 1 && s.includes(dynamicPropertyEntryIdWithSquareBrackets));
-  const dynamicPropertyName = dynamicPropertyNamePart.replace(dynamicPropertyEntryIdWithSquareBrackets, "");
+  const dynamicPropertyNamePart = key
+    .split(".")
+    .find(
+      (s, index) =>
+        index > 1 && s.includes(dynamicPropertyEntryIdWithSquareBrackets)
+    );
+  const dynamicPropertyName = dynamicPropertyNamePart.replace(
+    dynamicPropertyEntryIdWithSquareBrackets,
+    ""
+  );
 
   return Array.isArray(transferSetupModel[dynamicPropertyName]);
 };
@@ -25,19 +33,31 @@ const getDynamicZoneEntryId = (key, transferSetupModel) => {
 
   const matches = key.match(/\[\d+\]+/g);
   // remove square brackets
-  const id = matches[1].replace(/[\[\]']+/g, '');
+  const id = matches[1].replace(/[\[\]']+/g, "");
   return id;
-}
+};
+
+const getUniqueDynamicZoneEntryId = (key, transferSetupModel) => {
+  if (!isDynamicZoneKey(key, transferSetupModel)) {
+    return undefined;
+  }
+
+  const keySplittedByDot = key.split(".");
+
+  return keySplittedByDot[2];
+};
 
 const doesPickPathsIncludeTheComponent = (pickPaths, dzParameterKey) => {
   return pickPaths.some((pp) => pp.includes(dzParameterKey));
-}
+};
 
 const pickEntries = (flatten, pickPaths, transferSetupModel) => {
   // * dynamic zones properties must! always be in 1st level (not in component; Strapi restrictions)
 
   const dzEntryIdComponentMap = {};
-  const flattenComponentProps = Object.fromEntries(Object.entries(flatten).filter(([key]) => key.includes('__component')));
+  const flattenComponentProps = Object.fromEntries(
+    Object.entries(flatten).filter(([key]) => key.includes("__component"))
+  );
 
   const pickedDeep = pickDeep(transferSetupModel, ["__component__"]);
   const flattenedPickedDeep = flattenObject(pickedDeep);
@@ -52,25 +72,41 @@ const pickEntries = (flatten, pickPaths, transferSetupModel) => {
       return;
     }
 
-    const entryId = getDynamicZoneEntryId(key, transferSetupModel);
-    dzEntryIdComponentMap[entryId] = {
+    const keySplittedByDot = key.replace(".__component", "").split(".");
+    const componentName = keySplittedByDot[keySplittedByDot.length - 1].replace(
+      /\[.*\]/g,
+      ""
+    );
+    const uniqueEntryId = getUniqueDynamicZoneEntryId(key, transferSetupModel);
+
+    const keyOfEntry = Object.keys(flattenedPickedDeep)
+      .filter((fpdk) => fpdk.replace(/\[.*\]/g, "") == componentName)
+      .find((fpdk) => flattenedPickedDeep[fpdk] === flattenComponentProps[key]);
+    dzEntryIdComponentMap[uniqueEntryId] = {
       component: flattenComponentProps[key],
-      key: Object.keys(flattenedPickedDeep).find((fpdk) => flattenedPickedDeep[fpdk] === flattenComponentProps[key]),
+      key: keyOfEntry,
     };
   });
 
   const pickedEntries = {};
-  const mappedPickPaths = pickPaths.map((pickPath) => pickPath.replace(/\[\d+\]/g, ""));
+  const mappedPickPaths = pickPaths.map((pickPath) =>
+    pickPath.replace(/\[\d+\]/g, "")
+  );
 
   // filter out "__component"
-  const filteredFlatten = Object.fromEntries(Object.entries(flatten).filter(([key]) => !key.includes('__component')));
+  const filteredFlatten = Object.fromEntries(
+    Object.entries(flatten).filter(([key]) => !key.includes("__component"))
+  );
   Object.keys(filteredFlatten).forEach((key) => {
     // decide whether it is a dynamic zone
     // handle dynamic zones, in case of similarly-named fields across more components
     const isDynamicZone = isDynamicZoneKey(key, transferSetupModel);
     if (isDynamicZone) {
-      const dzEntryId = getDynamicZoneEntryId(key, transferSetupModel);
-      const dzParameterKey = dzEntryIdComponentMap[dzEntryId].key;
+      const uniqueDzEntryId = getUniqueDynamicZoneEntryId(
+        key,
+        transferSetupModel
+      );
+      const dzParameterKey = dzEntryIdComponentMap[uniqueDzEntryId].key;
 
       if (!doesPickPathsIncludeTheComponent(pickPaths, dzParameterKey)) {
         return;
@@ -81,11 +117,19 @@ const pickEntries = (flatten, pickPaths, transferSetupModel) => {
     if (mappedPickPaths.includes(filteredKey)) {
       if (isDynamicZone) {
         const dzEntryId = getDynamicZoneEntryId(key, transferSetupModel);
-        const dzParameterKey = dzEntryIdComponentMap[dzEntryId].key;
+        const uniqueDzEntryId = getUniqueDynamicZoneEntryId(
+          key,
+          transferSetupModel
+        );
+        const dzParameterKey = dzEntryIdComponentMap[uniqueDzEntryId].key;
 
-        const dzParameterName = dzParameterKey.substring(0, dzParameterKey.indexOf('['));
+        const dzParameterName = dzParameterKey.substring(
+          0,
+          dzParameterKey.indexOf("[")
+        );
         const toBeReplaced = `${dzParameterName}[${dzEntryId}]`;
-        const dzParameterComponent = dzEntryIdComponentMap[dzEntryId].component;
+        const dzParameterComponent =
+          dzEntryIdComponentMap[uniqueDzEntryId].component;
         const componentAddition = `${dzParameterName}[${dzEntryId};${dzParameterComponent}]`;
         const replacedKey = key.replace(toBeReplaced, componentAddition);
         pickedEntries[replacedKey] = flatten[key];
