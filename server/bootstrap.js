95c791e4f63610c965351cc60670ba11effdbfa7
david-vaclavek <vaclavek.dvd@gmail.com>
2023-02-28 14:58:52 +0100

    ✨ feat: allow "deprecate" feature

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -2,6 +2,7 @@
 
 const deepPopulateHook = require('./lifecycles/deep-populate-hook');
 const uploadEventEntryToLocalazyHook = require('./lifecycles/upload-event-entry-to-localazy-hook');
+const deprecateEventEntryInLocalazyHook = require('./lifecycles/deprecate-event-entry-in-localazy-hook');
 const RequestInitiatorHelper = require('./utils/request-initiator-helper');
 
 module.exports = ({ strapi }) => {
@@ -28,6 +29,24 @@ module.exports = ({ strapi }) => {
               strapi.log.info(`${action} hook result: ${JSON.stringify(result)}`);
             }
           });
+        } catch (e) {
+          strapi.log.error(e);
+        } finally {
+          break;
+        }
+      }
+      case 'beforeDeleteMany':
+      case 'beforeDelete': {
+        try {
+          if (requestInitiatorHelper.isInitiatedByLocalazyWebhook() || requestInitiatorHelper.isInitiatedByLocalazyPluginUI()) {
+            break;
+          }
+
+          // have to await here
+          const result = await deprecateEventEntryInLocalazyHook(event);
+          if (typeof result !== 'undefined') {
+            strapi.log.info(`${action} hook result: ${JSON.stringify(result)}`);
+          }
         } catch (e) {
           strapi.log.error(e);
         } finally {
