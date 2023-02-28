36c7b54587673ef04d108054474e5dadf2ddae17
david-vaclavek <vaclavek.dvd@gmail.com>
2023-02-28 12:59:41 +0100

    ♻️ refactor: move part of the Content Transfer Setup logic to common external function

--- admin/src/modules/plugin-settings/functions/build-content-transfer-setup-schema.js
+++ admin/src/modules/plugin-settings/functions/build-content-transfer-setup-schema.js
@@ -1,86 +1,26 @@
 import set from "lodash-es/set";
 import get from "lodash-es/get";
-import uniq from "lodash-es/uniq";
-import deepKeys from "@david-vaclavek/deep-keys";
-import sortByModelName from "../../@common/utils/sort-by-model-name";
-import arrayOfModelsToObject from "../utils/array-of-models-to-object";
 import findModelValueByKey from "../utils/find-model-value-by-key";
+import getContentTransferSetupKeysSets from "./get-content-transfer-setup-keys-sets";
 
 export default (
   localizableTree = [],
   storedSetupSchema = [],
   allModelsTree = []
 ) => {
-  localizableTree = arrayOfModelsToObject(
-    localizableTree.sort(sortByModelName)
-  );
-  storedSetupSchema = arrayOfModelsToObject(
-    storedSetupSchema.sort(sortByModelName)
-  );
-
-  const currentModelsSchemaKeys = deepKeys(localizableTree);
-  const storedSetupSchemaKeys = deepKeys(storedSetupSchema);
-
-  // components order may have changed; this would prevent properties from mixing up
-  const regex = /\.\d+\.__component__/;
-  let currentModelsSchemaComponentKeys = currentModelsSchemaKeys.filter((key) => key.match(regex)).map((key) => key.replace(regex, ''));
-  let storedSetupSchemaComponentKeys = storedSetupSchemaKeys.filter((key) => key.match(regex)).map((key) => key.replace(regex, ''));
-  currentModelsSchemaComponentKeys = uniq(currentModelsSchemaComponentKeys);
-  storedSetupSchemaComponentKeys = uniq(storedSetupSchemaComponentKeys);
-
-  const currentModelsSchemaComponentKeysComponentProp = currentModelsSchemaKeys.filter((key) => key.includes('__component__') && currentModelsSchemaComponentKeys.some((k) => key.includes(k)));
-  const storedSetupSchemaComponentKeysComponentProp = storedSetupSchemaKeys.filter((key) => key.includes('__component__') && storedSetupSchemaComponentKeys.some((k) => key.includes(k)));
-
-  /**
-   * Is in the "stored setup schema" but not in the "current model"
-   */
-  let removedKeys = [];
-  removedKeys.push(...storedSetupSchemaComponentKeysComponentProp.map((key) => {
-    const storedComponentName = get(storedSetupSchema, key);
-    const currentComponentName = get(localizableTree, key);
-
-    // this must return an array of keys
-    if (storedComponentName !== currentComponentName) {
-      const part = key.split('.').slice(0, -1).join('.');
-
-      return storedSetupSchemaKeys.filter((k) => k.includes(part));
-    }
-
-    return [];
-  }).reduce((acc, val) => acc.concat(val), []));
-  removedKeys.push(...storedSetupSchemaKeys.filter((key) => !currentModelsSchemaKeys.includes(key)));
-  removedKeys = uniq(removedKeys);
-
-  /**
-   * Is in the "current model" but not in the "stored setup schema"
-   */
-  let newKeys = [];
-  newKeys.push(...currentModelsSchemaComponentKeysComponentProp.map((key) => {
-    const storedComponentName = get(storedSetupSchema, key);
-    const currentComponentName = get(localizableTree, key);
-
-    // this must return an array of keys
-    if (storedComponentName !== currentComponentName) {
-      const part = key.split('.').slice(0, -1).join('.');
-
-      return currentModelsSchemaKeys.filter((k) => k.includes(part));
-    }
-
-    return [];
-  }).reduce((acc, val) => acc.concat(val), []));
-  newKeys.push(...currentModelsSchemaKeys.filter((key) => !storedSetupSchemaKeys.includes(key)));
-  newKeys = uniq(newKeys);
-
-  /**
-   * Filter out the removed keys
-  */
-  const filteredStoredSetupSchemaKeys = storedSetupSchemaKeys.filter(
-    (key) => !removedKeys.includes(key)
+  const {
+    oLocalizableTree,
+    oStoredSetupSchema,
+    newKeys,
+    filteredStoredSetupSchemaKeys
+  } = getContentTransferSetupKeysSets(
+    localizableTree,
+    storedSetupSchema,
   );
 
   const destinationObject = {};
   filteredStoredSetupSchemaKeys.forEach((key) => {
-    const localizableTreeValueToSet = get(storedSetupSchema, key);
+    const localizableTreeValueToSet = get(oStoredSetupSchema, key);
     const allModelsTreeValueToSet = findModelValueByKey(allModelsTree, key);
 
     if (allModelsTreeValueToSet === null) {
@@ -94,7 +34,7 @@ export default (
     }
   });
   newKeys.forEach((key) => {
-    set(destinationObject, key, get(localizableTree, key));
+    set(destinationObject, key, get(oLocalizableTree, key));
   });
 
   const destinationArray = [];
--- admin/src/modules/plugin-settings/functions/has-model-changed.js
+++ admin/src/modules/plugin-settings/functions/has-model-changed.js
@@ -1,12 +1,9 @@
-import deepKeys from "@david-vaclavek/deep-keys";
 import get from "lodash-es/get";
-import uniq from "lodash-es/uniq";
 import PluginSettingsService from "../services/plugin-settings-service";
 import StrapiModelService from "../services/strapi-model-service";
 import getModelsTree from "../utils/get-models-tree";
-import sortByModelName from "../../@common/utils/sort-by-model-name";
-import arrayOfModelsToObject from "../utils/array-of-models-to-object";
 import findModelValueByKey from "../utils/find-model-value-by-key";
+import getContentTransferSetupKeysSets from "./get-content-transfer-setup-keys-sets";
 
 /**
  * Pass parameters optionally so we don't have to fetch them again
@@ -37,83 +34,26 @@ export default async (
     allModelsTree = getModelsTree(models, models);
   }
 
-  localizableTree = arrayOfModelsToObject(
-    localizableTree.sort(sortByModelName)
+  const {
+    oStoredSetupSchema,
+    removedKeys,
+    newKeys,
+    filteredStoredSetupSchemaKeys
+  } = getContentTransferSetupKeysSets(
+    localizableTree,
+    storedSetupSchema,
   );
-  storedSetupSchema = arrayOfModelsToObject(
-    storedSetupSchema.sort(sortByModelName)
-  );
-
-  const currentModelsSchemaKeys = deepKeys(localizableTree);
-  const storedSetupSchemaKeys = deepKeys(storedSetupSchema);
-
-  // components order may have changed; this would prevent properties from mixing up
-  const regex = /\.\d+\.__component__/;
-  let currentModelsSchemaComponentKeys = currentModelsSchemaKeys.filter((key) => key.match(regex)).map((key) => key.replace(regex, ''));
-  let storedSetupSchemaComponentKeys = storedSetupSchemaKeys.filter((key) => key.match(regex)).map((key) => key.replace(regex, ''));
-  currentModelsSchemaComponentKeys = uniq(currentModelsSchemaComponentKeys);
-  storedSetupSchemaComponentKeys = uniq(storedSetupSchemaComponentKeys);
-
-  const currentModelsSchemaComponentKeysComponentProp = currentModelsSchemaKeys.filter((key) => key.includes('__component__') && currentModelsSchemaComponentKeys.some((k) => key.includes(k)));
-  const storedSetupSchemaComponentKeysComponentProp = storedSetupSchemaKeys.filter((key) => key.includes('__component__') && storedSetupSchemaComponentKeys.some((k) => key.includes(k)));
-
-  /**
-   * Is in the "stored setup schema" but not in the "current model"
-   */
-  let removedKeys = [];
-  removedKeys.push(...storedSetupSchemaComponentKeysComponentProp.map((key) => {
-    const storedComponentName = get(storedSetupSchema, key);
-    const currentComponentName = get(localizableTree, key);
-
-    // this must return an array of keys
-    if (storedComponentName !== currentComponentName) {
-      const part = key.split('.').slice(0, -1).join('.');
-
-      return storedSetupSchemaKeys.filter((k) => k.includes(part));
-    }
-
-    return [];
-  }).reduce((acc, val) => acc.concat(val), []));
-  removedKeys.push(...storedSetupSchemaKeys.filter((key) => !currentModelsSchemaKeys.includes(key)));
-  removedKeys = uniq(removedKeys);
-
-  /**
-   * Is in the "current model" but not in the "stored setup schema"
-   */
-  let newKeys = [];
-  newKeys.push(...currentModelsSchemaComponentKeysComponentProp.map((key) => {
-    const storedComponentName = get(storedSetupSchema, key);
-    const currentComponentName = get(localizableTree, key);
-
-    // this must return an array of keys
-    if (storedComponentName !== currentComponentName) {
-      const part = key.split('.').slice(0, -1).join('.');
-
-      return currentModelsSchemaKeys.filter((k) => k.includes(part));
-    }
-
-    return [];
-  }).reduce((acc, val) => acc.concat(val), []));
-  newKeys.push(...currentModelsSchemaKeys.filter((key) => !storedSetupSchemaKeys.includes(key)));
-  newKeys = uniq(newKeys);
 
   if (newKeys.length || removedKeys.length) {
     return true;
   }
 
   /**
-   * Filter out the removed keys
-  */
-  const filteredStoredSetupSchemaKeys = storedSetupSchemaKeys.filter(
-    (key) => !removedKeys.includes(key)
-  );
-
-  /**
    * Check if the values are different (type might have changed; boolean to null or vice versa)
    */
   // eslint-disable-next-line no-restricted-syntax
   for (const key of filteredStoredSetupSchemaKeys) {
-    const localizableTreeValueToSet = get(storedSetupSchema, key);
+    const localizableTreeValueToSet = get(oStoredSetupSchema, key);
     const allModelsTreeValueToSet = findModelValueByKey(allModelsTree, key);
 
     if (
