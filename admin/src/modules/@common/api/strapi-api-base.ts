b7b21fd1ec96ec00721a4c02d63af0c7e81ddd0f
david-vaclavek <vaclavek.dvd@gmail.com>
2025-03-31 15:48:38 +0200

    Refactor token retrieval in Strapi API base
    - Updated the `getToken` function to prioritize JWT token retrieval from cookies, falling back to localStorage and sessionStorage.
    - Removed unnecessary checks and parsing logic for improved clarity and efficiency.

--- admin/src/modules/@common/api/strapi-api-base.ts
+++ admin/src/modules/@common/api/strapi-api-base.ts
@@ -6,22 +6,16 @@ import axios from 'axios';
 import { PLUGIN_ID } from '../../../pluginId';
 
 const BASE_PLUGIN_PATH = `/${PLUGIN_ID}`;
+const JWT_TOKEN_NAME = 'jwtToken';
 
 const getToken = (): string => {
-  const token = localStorage.getItem('jwtToken') || sessionStorage.getItem('jwtToken') || '';
-  if (!token) {
-    return '';
-  }
-
-  const parsedToken = JSON.parse(token);
-  return parsedToken;
+  const jwtTokenCookie = document.cookie.split('; ').find((row) => row.startsWith(`${JWT_TOKEN_NAME}=`));
+  const jwtToken = jwtTokenCookie ? jwtTokenCookie.split('=')[1] : '';
+  return jwtToken || localStorage.getItem(JWT_TOKEN_NAME) || sessionStorage.getItem(JWT_TOKEN_NAME) || '';
 };
 
 const createStrapiApiAxiosInstance = (baseUrl: string | null = null) => {
   const token = getToken();
-  if (!token) {
-    // throw new Error("No token found");
-  }
 
   if (baseUrl === null) {
     baseUrl = `${BASE_PLUGIN_PATH}`;
@@ -38,6 +32,7 @@ const createStrapiApiAxiosInstance = (baseUrl: string | null = null) => {
         'Content-Type': 'application/json',
         // Add this header to identify the request as initiated by the plugin
         'X-Localazy-Initiated-By': 'strapi-plugin-localazy',
+        'x-hovno-do-riti': 'x-hovno-do-riti',
       });
       if (token) {
         config.headers.set('Authorization', `Bearer ${token}`);
